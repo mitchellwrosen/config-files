799c3a14bb9358cfc22ab097f3f43a3ad4bc4486
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-03-15 13:31:15 -0700

    changes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE TypeOperators #-}
+
 import System.IO (hPutStrLn)
 
 import XMonad
 import XMonad.Actions.CycleWS           (nextWS, prevWS, shiftToPrev, shiftToNext)
 import XMonad.Actions.DynamicWorkspaces (addWorkspacePrompt, removeEmptyWorkspace)
-import XMonad.Actions.Search            (google, promptSearchBrowser)
+import XMonad.Actions.Search
+import XMonad.Actions.Submap
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks         (avoidStruts, docksEventHook, manageDocks)
+import XMonad.Hooks.ManageDocks         (AvoidStruts, avoidStruts, docksEventHook, manageDocks)
 import XMonad.Hooks.SetWMName
-import XMonad.Layout.Accordion          (Accordion(..))
+import XMonad.Layout.LayoutModifier     (ModifiedLayout)
+import XMonad.Layout.Maximize           (Maximize, maximize, maximizeRestore)
+import XMonad.Layout.Spacing
 import XMonad.Prompt
 import XMonad.Util.Cursor
 import XMonad.Util.Run                  (safeSpawn, spawnPipe)
@@ -19,7 +24,7 @@ main = do
    xmproc <- spawnPipe "~/.xmonad/bin/xmobar ~/.xmonad/.xmobarrc"
    xmonad $ def
       { terminal           = "urxvt"
-      , workspaces         = ["α", "β", "γ", "δ"]
+      , workspaces         = ["α", "β", "γ", "δ", "ε", "ζ"]
       , borderWidth        = 2
       , focusFollowsMouse  = False
       , modMask            = mod4Mask -- Rebind Mod to the Windows key
@@ -40,8 +45,18 @@ main = do
       `removeKeysP` removeKeys'
       `additionalKeysP` additionalKeys'
 
-layout_hook = avoidStruts $
-  layoutHook def ||| Accordion
+type (:+) f g = Choose f g
+infixr 5 :+
+
+layout_hook
+  :: ModifiedLayout AvoidStruts
+       (ModifiedLayout Maximize
+         (ModifiedLayout SmartSpacing (Tall :+ Mirror Tall :+ Full)))
+     Window
+layout_hook = modify (tall ||| Mirror tall ||| Full)
+ where
+  modify = avoidStruts . maximize . smartSpacing 5
+  tall = Tall 1 (3/100) (1/2)
 
 startup_hook :: X ()
 startup_hook = do
@@ -56,10 +71,16 @@ removeKeys' = [ "M-S-<Return>" -- terminal
               , "M-h"          -- shrink
               , "M-l"          -- expand
               , "M-<Return>"   -- swap master
+              , "M-m"          -- focus master
               ]
 
 xpconfig :: XPConfig
-xpconfig = amberXPConfig
+xpconfig = greenXPConfig
+  { font = "-misc-fixed-*-*-*-*-20-*-*-*-*-*-*-*"
+  , height = 26
+  , historySize = 0
+  , promptBorderWidth = 1
+  }
 
 additionalKeys' :: [(String, X ())]
 additionalKeys' =   -- Window/workspace management
@@ -73,10 +94,29 @@ additionalKeys' =   -- Window/workspace management
                   , ("M-M1-h",       sendMessage Shrink)
                   , ("M-M1-l",       sendMessage Expand)
                   -- Shell/browser
-                  , ("M-<Return>",   spawn "urxvt") -- TODO: don't hard-code
+                  , ("M-<Return>",   spawn =<< asks (terminal . config))
                   , ("M-i",          spawn "google-chrome-stable")
                   -- Search
-                  , ("M-o",          promptSearchBrowser xpconfig "google-chrome-unstable" google)
+                  --, ("M-o",          promptSearchBrowser xpconfig "google-chrome-unstable" google)
+                  , ("M-o g", promptSearch xpconfig google)
+                  , ("M-o h", promptSearch xpconfig hackage)
                   -- Shutdown
                   , ("M-S-<Delete>", spawn "sudo shutdown")
+
+                  , ("M-m", withFocused (sendMessage . maximizeRestore))
+                  -- , ("M-M1-j", sendMessage (ModifySpacing (+1)))
                   ]
+
+-- Like promptSearchBrowser, but open it up so I have access to the flags to
+-- pass to the browser. This lets me pass "--new-window" to chrome, so my
+-- searches don't appear in new tabs on some random existing browser window.
+promptSearchBrowser' :: XPConfig -> Browser -> SearchEngine -> X ()
+promptSearchBrowser' config browser (SearchEngine name site) =
+    mkXPrompt (Search' name) config (historyCompletionP ("Search [" `isPrefixOf`))
+      (\query -> safeSpawn browser ["--new-window", site query])
+
+data Search' = Search' Name
+instance XPrompt Search' where
+    showXPrompt (Search' name)= "Search [" ++ name ++ "]: "
+    nextCompletion _ = getNextCompletion
+    commandToComplete _ c = c
